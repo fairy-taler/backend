8f488bff4bb03975b7d7c37860b648c5f4f3540c
LeeYuri <56033943+lyr426@users.noreply.github.com>
2022-11-04 17:41:58 +0900

    #20 Add 목록조회

--- src/main/java/com/fairytaler/fairytalecat/tale/domain/repository/TaleRepository.java
+++ src/main/java/com/fairytaler/fairytalecat/tale/domain/repository/TaleRepository.java
@@ -1,7 +1,10 @@
 package com.fairytaler.fairytalecat.tale.domain.repository;
 
+import com.fairytaler.fairytalecat.mongoTest.model.MongoDBTestModel;
 import com.fairytaler.fairytalecat.tale.domain.model.Tale;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface TaleRepository extends MongoRepository<Tale, String>{}
+public interface TaleRepository extends MongoRepository<Tale, String>{
+    Tale findByMemberCode(String memberCode);
+}
 
