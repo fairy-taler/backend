67165c84cae9434c171a5469fe8c6c0ce12d5848
jihi32123 <jihi32123@gmail.com>
2022-11-04 17:41:58 +0900

    #19 Add 동화 저장 api

--- src/main/java/com/fairytaler/fairytalecat/mongoTest/controller/MongoDBTestController.java
+++ src/main/java/com/fairytaler/fairytalecat/mongoTest/controller/MongoDBTestController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 @Slf4j
 @RestController
 @RequestMapping(path = "/mongo")
