805e31da4ffa8a750d9d78e919d4ab974006e16b
jihi32123 <jihi32123@gmail.com>
2022-11-06 19:40:14 +0900

    #40 Add FAQ 비공개 설정 API

--- src/main/java/com/fairytaler/fairytalecat/community/command/application/controller/FaqController.java
+++ src/main/java/com/fairytaler/fairytalecat/community/command/application/controller/FaqController.java
@@ -7,10 +7,7 @@ import com.fairytaler.fairytalecat.community.command.application.service.FaqServ
 import com.fairytaler.fairytalecat.community.command.domain.model.Faq;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FaqController {
@@ -34,4 +31,10 @@ public class FaqController {
         }
         return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "FAQ 수정 성공", faq));
     }
+    /* FAQ 비공개*/
+    @PutMapping("/faq/{faqCode}")
+    public ResponseEntity<ResponseDTO> updateNoticeToPublic(@PathVariable Long faqCode, @RequestParam(name="isPublic", defaultValue="true") boolean isPublic){
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "공지사항 비공개 설정 성공",  faqService.updateFaqToPublic(faqCode, isPublic)));
+    }
+
 }
