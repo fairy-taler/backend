bcbc45426202e6ccff79b4a57b3d9838cfa5ee41
jihi32123 <jihi32123@gmail.com>
2022-11-06 17:11:46 +0900

    Refactor notice controller 코드 수정

--- src/main/java/com/fairytaler/fairytalecat/community/query/application/controller/NoticeQueryController.java
+++ src/main/java/com/fairytaler/fairytalecat/community/query/application/controller/NoticeQueryController.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class NoticeQueryController {
     static private TokenProvider tokenProvider;
-    static private NoticeQueryService noticeService;
-    public NoticeQueryController(NoticeQueryService noticeService, TokenProvider tokenProvider){
+    static private NoticeQueryService noticeQueryService;
+    public NoticeQueryController(NoticeQueryService noticeQueryService, TokenProvider tokenProvider){
         this.tokenProvider = tokenProvider;
-        this.noticeService = noticeService;
+        this.noticeQueryService = noticeQueryService;
     }
 
     /* 공지사항 조회 */
@@ -23,16 +23,16 @@ public class NoticeQueryController {
         /* role 확인 후, 관리자면 전체 조회, 일반 유저면 public 데이터만 조회*/
 
         /* 공지사항이 있으면 조회 후 반환 */
-        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "공지사항 조회 성공", noticeService.getNotice(noticeCode)));
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "공지사항 조회 성공", noticeQueryService.getNotice(noticeCode)));
     }
 
     /* 공지사항 전체 조회 */
     @GetMapping("/notices")
-    public ResponseEntity<ResponseDTO> selectNoticeListWithPaging(@RequestParam(name="isPublic", defaultValue="N") String isPublic){
+    public ResponseEntity<ResponseDTO> selectNoticeListWithPaging(Pageable pageable){
         /* role 확인 후, 관리자면 전체 조회, 일반 유저면 public 데이터만 조회*/
 
         /* 공지사항이 있으면 조회 후 반환 */
-        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "공지사항 조회 성공", isPublic));
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "공지사항 조회 성공", noticeQueryService.getNoticeListWidthPaging(pageable)));
     }
 
 }
