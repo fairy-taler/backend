8c22252debd55b1edd4a3951d2ad11b6a9d6aa91
jihi32123 <jihi32123@gmail.com>
2022-11-06 17:11:46 +0900

    Refactor notice controller 코드 수정

--- src/main/java/com/fairytaler/fairytalecat/community/query/application/controller/FaqQueryController.java
+++ src/main/java/com/fairytaler/fairytalecat/community/query/application/controller/FaqQueryController.java
@@ -3,10 +3,12 @@ package com.fairytaler.fairytalecat.community.query.application.controller;
 import com.fairytaler.fairytalecat.common.response.ResponseDTO;
 import com.fairytaler.fairytalecat.community.command.domain.model.Faq;
 import com.fairytaler.fairytalecat.community.query.application.service.FaqQueryService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Optional;
@@ -28,5 +30,14 @@ public class FaqQueryController {
             return  ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "FAQ 조회 실패",faqCode ));
         }
         return  ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "FAQ 조회 성공",faq));
+    }
+
+    /* 전체 조회 */
+    @GetMapping("/faq")
+    public ResponseEntity<ResponseDTO> selectFaqListWithPaging(Pageable pageable){
+        /* role 확인 후, 관리자면 전체 조회, 일반 유저면 public 데이터만 조회*/
+
+        /* 공지사항이 있으면 조회 후 반환 */
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "공지사항 조회 성공", pageable));
     }
 }
