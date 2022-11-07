3b6a877ead4c3c123cff88cbc3dee6c44261bcab
jihi32123 <jihi32123@gmail.com>
2022-11-07 13:38:20 +0900

    #41 Add 1대1문의 등록

---
+++ src/main/java/com/fairytaler/fairytalecat/community/command/application/service/InquiryService.java
@@ -0,0 +1,35 @@
+package com.fairytaler.fairytalecat.community.command.application.service;
+
+import com.fairytaler.fairytalecat.community.command.application.dao.InquiryDAO;
+import com.fairytaler.fairytalecat.community.command.application.dto.InquiryRequestDTO;
+import com.fairytaler.fairytalecat.community.command.domain.model.Faq;
+import com.fairytaler.fairytalecat.community.command.domain.model.Inquiry;
+import com.fairytaler.fairytalecat.jwt.TokenProvider;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class InquiryService {
+    static private InquiryDAO inquiryDAO;
+    static private TokenProvider tokenProvider;
+
+    public InquiryService(InquiryDAO inquiryDAO, TokenProvider tokenProvider){
+        this.inquiryDAO = inquiryDAO;
+        this.tokenProvider = tokenProvider;
+    }
+    public Inquiry insertInquiry(String accessToken, InquiryRequestDTO inquiryRequestDTO) {
+        /* 데이터 생성 */
+        Inquiry inquiry = new Inquiry();
+        inquiry.setTitle(inquiryRequestDTO.getTitle());
+        inquiry.setContent(inquiryRequestDTO.getContent());
+        inquiry.setCreateDate(new Date());
+
+        /* 사용자 정보 (작성자) 가져와서 넣기 */
+        String memberCode = tokenProvider.getUserCode(accessToken);
+        inquiry.setMemberCode(Long.parseLong(memberCode));
+        inquiryDAO.save(inquiry);
+
+        return inquiry;
+    }
+}
