278de1835fa1a846e6c07ff9defd6f1bf27a55e0
lyr426 <leeyr426@gmail.com>
2022-11-09 14:15:03 +0900

    #54 Add 회원 비밀번호 변경api

--- src/main/java/com/fairytaler/fairytalecat/member/command/application/service/MemberService.java
+++ src/main/java/com/fairytaler/fairytalecat/member/command/application/service/MemberService.java
@@ -1,12 +1,17 @@
 package com.fairytaler.fairytalecat.member.command.application.service;
 
 import com.fairytaler.fairytalecat.avatar.domain.repository.AvatarRepository;
+import com.fairytaler.fairytalecat.exception.LoginFailedException;
 import com.fairytaler.fairytalecat.jwt.TokenProvider;
 import com.fairytaler.fairytalecat.member.command.application.dao.MemberMapper;
+import com.fairytaler.fairytalecat.member.command.application.dto.MemberDTO;
 import com.fairytaler.fairytalecat.member.domain.model.Member;
+import com.fairytaler.fairytalecat.member.domain.model.MemberPwd;
 import com.fairytaler.fairytalecat.member.domain.repository.MemberInfoRepository;
+import com.fairytaler.fairytalecat.member.domain.repository.MemberPwdRepository;
 import com.fairytaler.fairytalecat.member.domain.repository.MemberRepository;
 import com.fairytaler.fairytalecat.member.query.apllication.dto.RequestMemberInfoDTO;
+import com.fairytaler.fairytalecat.member.query.apllication.dto.RequestUpdatePwdDTO;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -20,13 +25,15 @@ public class MemberService {
     private final TokenProvider tokenProvider;
     private final MemberRepository memberRepository;
     private final MemberInfoRepository memberInfoRepository;
+    private final MemberPwdRepository memberPwdRepository;
 
-    public MemberService(MemberMapper memberMapper, PasswordEncoder passwordEncoder, TokenProvider tokenProvider, MemberRepository memberRepository, AvatarRepository avatarRepository, MemberInfoRepository memberInfoRepository) {
+    public MemberService(MemberMapper memberMapper, PasswordEncoder passwordEncoder, TokenProvider tokenProvider, MemberRepository memberRepository, AvatarRepository avatarRepository, MemberInfoRepository memberInfoRepository, MemberPwdRepository memberPwdRepository) {
         this.memberMapper = memberMapper;
         this.passwordEncoder = passwordEncoder;
         this.tokenProvider = tokenProvider;
         this.memberRepository = memberRepository;
         this.memberInfoRepository = memberInfoRepository;
+        this.memberPwdRepository = memberPwdRepository;
     }
 
     public Member updateMemberInfo(String accessToken, RequestMemberInfoDTO requestMemberInfoDTO) {
@@ -47,5 +54,25 @@ public class MemberService {
             return null;
         }
 
+    }
+
+    public String updatePwd(String accessToken, RequestUpdatePwdDTO requestUpdatePwdDTO) {
+        String memberId = tokenProvider.getUserId(accessToken);
+        Optional<MemberDTO> member = memberMapper.findByMemberId(memberId);
+        if(!passwordEncoder.matches(requestUpdatePwdDTO.getOriginalPwd(),member.get().getMemberPwd())){
+            throw new LoginFailedException("잘못된 비밀번호 입니다.");
+        }
+        Optional<MemberPwd> optionalMember = memberPwdRepository.findById(memberId);
+
+        try{
+            MemberPwd updateMember = optionalMember.get();
+            updateMember.setMemberPwd(passwordEncoder.encode(requestUpdatePwdDTO.getNewPwd()));
+            memberPwdRepository.save(updateMember);
+        }
+        catch (Exception exception){
+            return null;
+        }
+
+        return memberId;
     }
 }
